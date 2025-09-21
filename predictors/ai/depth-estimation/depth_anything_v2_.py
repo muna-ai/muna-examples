7b52bfec8c34f09778ef7eeabaf586d9290f2e56
Yusuf Olokoba <olokobayusuf@gmail.com>
2025-09-21 15:14:44 -0400

    Add depth anything v2

--- predictors/ai/depth-estimation/depth_anything_v2_.py
+++ predictors/ai/depth-estimation/depth_anything_v2_.py
@@ -3,8 +3,8 @@
 #   Copyright © 2025 NatML Inc. All Rights Reserved.
 #
 
-# NOTE: In order to run and/or compile this model, you must clone Depth Anything into the current working directory.
-# Run `git clone https://github.com/LiheYoung/Depth-Anything.git` then run/compile this script.
+# NOTE: In order to run and/or compile this model, you must clone Depth Anything V2 into the current working directory.
+# Run `git clone https://github.com/DepthAnything/Depth-Anything-V2.git` then run/compile this script.
 
 # /// script
 # requires-python = ">=3.11"
@@ -17,34 +17,39 @@
 # ///
 
 from cv2 import applyColorMap, cvtColor, COLOR_BGR2RGB, COLORMAP_INFERNO
+from huggingface_hub import hf_hub_download
 from muna import compile, Parameter, Sandbox
 from muna.beta import OnnxRuntimeInferenceMetadata
 from numpy import ndarray, uint8
 from pathlib import Path
 from PIL import Image
 import sys
-from torch import inference_mode, randn
+from torch import inference_mode, load as torch_load, randn
 from torchvision.transforms import functional as F
 from typing import Annotated
 
-# Import Depth Anything
-sys.path.insert(0, str(Path.cwd() / "Depth-Anything"))
-from depth_anything.dpt import DepthAnything
+# Import Depth Anything V2
+sys.path.insert(0, str(Path.cwd() / "Depth-Anything-V2"))
+from depth_anything_v2.dpt import DepthAnythingV2
 
-# Create model
-model = DepthAnything.from_pretrained(
-    "LiheYoung/depth_anything_vitl14",
-    config={ "localhub": False }
-).eval()
+# Grab the DepthAnythingV2 vits/small model from HF
+weights_path = hf_hub_download(
+    repo_id="depth-anything/Depth-Anything-V2-Small",
+    filename="depth_anything_v2_vits.pth"
+)
+weights = torch_load(weights_path, map_location="cpu")
+model = DepthAnythingV2(encoder="vits", features=64, out_channels=[48, 96, 192, 384])
+model.load_state_dict(weights)
+model.eval()
 
 @compile(
-    tag="@tiktok/depth-anything-large",
-    description="Estimate metric depth from an image using Depth Anything (large).",
+    tag="@tiktok/depth-anything-v2-small",
+    description="Estimate metric depth from an image with Depth Anything V2 (small).",
     access="public",
     sandbox=Sandbox()
         .pip_install("torch", "torchvision", index_url="https://download.pytorch.org/whl/cpu")
         .pip_install("huggingface_hub", "opencv-python-headless")
-        .run_commands("git clone https://github.com/LiheYoung/Depth-Anything.git"),
+        .run_commands("git clone https://github.com/DepthAnything/Depth-Anything-V2.git"),
     metadata=[
         OnnxRuntimeInferenceMetadata(model=model, model_args=[randn(1, 3, 224, 224)]) # INCOMPLETE # Dynamic shapes??
     ]
@@ -54,9 +59,9 @@ def estimate_depth(
     image: Annotated[Image.Image, Parameter.Generic(description="Input image.")]
 ) -> Annotated[ndarray, Parameter.Generic(description="Metric depth tensor with shape (H,W).")]:
     """
-    Estimate metric depth from an image using Depth Anything (large).
+    Estimate metric depth from an image with Depth Anything V2 (small).
     """
-    # Preprocess image
+    # Preprocess
     src_width, src_height = image.size
     dst_width, dst_height = _get_resize_dimensions(
         image,
@@ -67,8 +72,8 @@ def estimate_depth(
     image = F.resize(image, (dst_height, dst_width))
     image_tensor = F.to_tensor(image)
     normalized_tensor = F.normalize(
-        image_tensor, 
-        mean=[0.485, 0.456, 0.406], 
+        image_tensor,
+        mean=[0.485, 0.456, 0.406],
         std=[0.229, 0.224, 0.225]
     )
     # Predict depth
@@ -117,8 +122,8 @@ def _visualize_depth(depth: ndarray) -> Image.Image:
 
 if __name__ == "__main__":
     # Predict
-    image = Image.open("media/city.jpg")
+    image = Image.open(f"media/city.jpg")
     depth = estimate_depth(image)
     # Visualize
-    depth_img = _visualize_depth(depth)
-    depth_img.show()
+    depth_image = _visualize_depth(depth)
+    depth_image.show()
