0ac5014d0bd811382b2cdd9eb76a19fe2dd97507
Yusuf Olokoba <olokobayusuf@gmail.com>
2025-11-12 12:40:30 -0800

    Minor update in `hoist_nested_variable`

--- python-coverage/hoist_nested_variable.py
+++ python-coverage/hoist_nested_variable.py
@@ -3,15 +3,16 @@
 #   Copyright © 2025 NatML Inc. All Rights Reserved.
 #
 
-# One major difference between Python and C++ is the topics of (block) scopes.
+# One major difference between Python and C++ is how each language handles scopes.
 #
 # In Python, there is no concept of scopes within a function. If you define a variable 
 # within an indented block (e.g. `if` or `while` statement body), the variable remains 
 # accessible outside that scope.
 #
 # But in C++, variables defined within a scope cannot be accessed outside that scope.
-# As such, we simply hoist all variables that are accessed outside their defining scope; 
-# and promote their types from `T` to `std::optional<T>`.
+# So to emulate Python's behaviour in C++, we simply hoist all variables that are 
+# accessed outside their defining scope. We then promote the type of the hoisted 
+# variable accordingly (e.g. `T` becomes `std::optional<T>`).
 
 from muna import compile
 import numpy as np
